95ca6a723b6d8e6c00eac28dd71ae60adc3d3e94
smoothdesign <ben.ndui@gmail.com>
2021-08-23 17:14:48 +0200

    Firebase setup [CHECKED] - Product controller [CHECKED] - Event controller [EN COURS]

---
+++ android/app/src/main/kotlin/com/smoothndesign/suividevente/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smoothndesign.suividevente
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/controller/FirebaseStorage/storage.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+
+class FireStorageService{
+
+  var uid;
+
+  FireStorageService({this.uid});
+
+  Future<dynamic> loadImage(BuildContext context, String image) async {
+    return await FirebaseStorage.instance.ref("images").child(image).getDownloadURL();
+  }
+}
